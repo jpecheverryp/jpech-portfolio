41c0f4dd6a2c3eb9e3207df67ed361079a99ed83
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2026-04-22 16:25:07 -0400

    removed Pico, add Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   redirects: {
     "/github": "https://github.com/jpecheverryp",
     "/linkedin": "https://www.linkedin.com/in/jpechdev/",
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
   }
 });
